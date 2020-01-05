83133c83a2546cbc19ba38383ad0ff776578ef06
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-05 01:44:34 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,11 +1,11 @@
 class Appointment
-  attr_accessor :date,:patient, :doctor
+  attr_accessor :patient, :date, :doctor
   
   @@all = []
 
   def initialize(date,patient, doctor)
-    @date = date
     @patient = patient
+    @date = date
     @doctor = doctor
     @@all << self
   end
