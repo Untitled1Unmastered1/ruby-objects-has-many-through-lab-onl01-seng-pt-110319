386654657b2d60f7368d810e2523c4854d8f171d
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-05 03:11:16 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,50 +1,27 @@
+require 'pry'
 class Doctor
-  attr_accessor :name, :add_appointment
+
+  attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
-    @appointments = []
-  end
-
-  def add_appointment(appointment)
-    @appointments << appointment
-    appointment.doctor = self
+    @@all << self
   end
 
   def appointments
-    @appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+  
+  def patients
+    appointments.select {|appointment| appointment.patient}
   end
 
-  def patients
-    @appointments.collect do |appointment|
-      appointment.patient
-    end
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
   end
 end
-# require 'pry'
-# class Doctor
-
-#   attr_accessor :name
-#   @@all = []
-
-#   def initialize(name)
-#     @name = name
-#     @@all << self
-#   end
-
-#   def appointments
-#     Appointment.all.select {|appointment| appointment.doctor == self}
-#   end
-  
-#   def patients
-#     appointments.select {|appointment| appointment.patient}
-#   end
-
-#   def self.all
-#     @@all
-#   end
-
-#   def new_appointment(patient, date)
-#     Appointment.new(patient, date, self)
-#   end
-# end
