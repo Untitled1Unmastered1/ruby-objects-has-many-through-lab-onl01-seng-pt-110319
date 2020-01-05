c03a4c499b2649c81cfcb70ba2d92c77aa64b9c0
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-05 03:42:12 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,7 +12,11 @@ class Patient
   def self.all
     @@all
   end
-
+  
+  def new_appointment(date, doctor)
+    Appointment.new(date, doctor, self)
+  end
+  
   def appointments
     Appointment.all.select {|appointment| appointment.patient == self}
   end
@@ -20,8 +24,5 @@ class Patient
   def doctors
     appointments.collect {|appointment| appointment.doctor}
   end
-
-  def new_appointment(date, doctor)
-    Appointment.new(date, doctor, self)
-  end
+  
 end
