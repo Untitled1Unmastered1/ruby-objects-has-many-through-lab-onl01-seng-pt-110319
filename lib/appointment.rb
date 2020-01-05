6cf0784833eba82d7f0526c3f7fbdafebe9a64ba
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-05 03:31:41 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,17 @@ class Appointment
   
   @@all = []
   
-  def initialize 
+  def initialize(date = " ",patient,doctor)
+    @date = date
+    @patient = patient 
+    @doctor = doctor 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+end 
 
 
 
