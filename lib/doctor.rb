5c05635ee1edc14dae38dca610af9b8dc3317ba3
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-05 02:01:38 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -21,7 +21,7 @@ class Doctor
   end
 end 
 
-#   def new_appointment(patient, date)
-#     Appointment.new(patient, date, self)
-#   end
-# end
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
+end
