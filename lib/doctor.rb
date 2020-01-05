10ed761f3c419e5c74f2545ec0a2d417861ec4f3
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-05 02:47:49 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,57 +1,26 @@
 class Doctor
 
-  attr_reader :name, :appointment, :patient
-
-  @@all_doc = []
+  attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
-    @@all_doc << self
-  end
-
-  def self.all
-    @@all_doc
-  end
-
-  def new_appointment(patient, date)
-    Appointment.new(patient, self, date)
+    @@all << self
   end
 
   def appointments
-    Appointment.all.select do |appointment|
-      appointment.doctor == self
-    end
+    Appointment.all.select {|appointment| appointment.doctor == self}
   end
 
   def patients
-    appointments.map do |appointment|
-      appointment.patients
-    end
+    appointments.map {|appointment| appointment.patient}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
   end
 end
-# class Doctor
-
-#   attr_accessor :name
-#   @@all = []
-
-#   def initialize(name)
-#     @name = name
-#     @@all << self
-#   end
-
-#   def appointments
-#     Appointment.all.select {|appointment| appointment.doctor == self}
-#   end
-
-#   def patients
-#     appointments.map {|appointment| appointment.patient}
-#   end
-
-#   def self.all
-#     @@all
-#   end
-
-#   def new_appointment(patient, date)
-#     Appointment.new(patient, date, self)
-#   end
-# end
