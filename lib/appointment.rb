c5cf4b463d61da91dfe8cfa8a22a8feea1c54f50
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-05 02:47:26 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,43 +1,17 @@
 require 'pry'
 class Appointment
-
-  attr_reader :patient, :doctor, :date
-
+  attr_accessor :date, :patient, :doctor
+  
   @@all = []
 
-  def initialize(doctor = "", patient ="", date = "")
+  def initialize(date,patient, doctor)
     @patient = patient
-    @doctor = doctor
     @date = date
+    @doctor = doctor
     @@all << self
   end
 
   def self.all
     @@all
   end
-
-  def patients
-    self.patient
-  end
-
-  def doctors
-    self.doctor
-  end
 end
-# require 'pry'
-# class Appointment
-#   attr_accessor :date, :patient, :doctor
-  
-#   @@all = []
-
-#   def initialize(date,patient, doctor)
-#     @patient = patient
-#     @date = date
-#     @doctor = doctor
-#     @@all << self
-#   end
-
-#   def self.all
-#     @@all
-#   end
-# end
